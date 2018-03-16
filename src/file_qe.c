c98fd1c500b9c8cafadb9f5ae691f6cc35f4b2db
ovhpa <hubert.valencia@imass.nagoya-u.ac.jp>
2018-03-16 14:42:44 +0900

    file_qe: classic typo m(_ _)m

--- src/file_qe.c
+++ src/file_qe.c
@@ -584,7 +584,7 @@ while (TRUE)
   /* energy */
   if (g_strrstr(line, "total energy") != NULL)
     {/* _BUG_ this will catch "The total energy is the sum of (...)" line as well*/
- if(g_strrstr(line, "total energy is the sum of") == NULL){/* FIX: exclude this line */
+ if(g_strrstr(line, "total energy is the sum of") == NULL)/* FIX: exclude this line */
     {
     buff = g_strsplit(line, "=", 2);
     text = format_value_and_units(*(buff+1), 5);
