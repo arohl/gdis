514da97c325248ca7e39d8fdc4d1cedca08d17c2
Craig Fisher <66772456+cajfisher@users.noreply.github.com>
2024-02-22 10:26:41 +0900

    Check GLIB version
    Some OSs use older versions of glib, before g_pattern_spec_match_string replaced g_pattern_match_string (deprecated since v2.70)

--- src/file.c
+++ src/file.c
@@ -33,6 +33,12 @@ The GNU GPL can also be found at http://www.gnu.org
 #include <sys/stat.h>
 #include <sys/types.h>
 
+#define GLIB_VERSION(major, minor, micro) \
+    (GLIB_MAJOR_VERSION > (major) || \
+    (GLIB_MAJOR_VERSION == (major) && GLIB_MINOR_VERSION > (minor)) || \
+    (GLIB_MAJOR_VERSION == (major) && GLIB_MINOR_VERSION == (minor) && \
+     GLIB_MICRO_VERSION >= (micro)))
+
 #include "gdis.h"
 #include "coords.h"
 #include "error.h"
@@ -801,7 +807,11 @@ while (list)
   name = list->data;
   list = g_slist_next(list);
 
+#if GLIB_VERSION(2, 7, 0)
   if (!g_pattern_spec_match_string(ps, name))
+#else
+  if (!g_pattern_match_string(ps, name))
+#endif
     {
     files = g_slist_remove(files, name);
     g_free(name);
